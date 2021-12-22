2dbd52e02d033d4e2ae2aeb72d326bdd767f0b7a
Łazarz <mlazarz@pl.imshealth.com>
2021-12-22 15:32:37 +0100

    firsst

---
+++ courses/views.py
@@ -0,0 +1,10 @@
+from django.views.generic.list import ListView
+from .models import Course
+
+class ManageCourseListView(ListView):
+	model = Course
+	template_name = 'courses/manage/course/list.html'
+
+	def get_queryet(self):
+		qs = super(ManageCourseListView, self).get_queryet()
+		return qs.filter(owner=self.request.user)
